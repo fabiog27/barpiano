f8ba6205cfe9bad6003585a940293eb7950c0135
Fabio Greter <fabio.greter@bluewin.ch>
2022-09-12 15:52:50 +0200

    MAKE FASTER

--- helpers/serial_connection.py
+++ helpers/serial_connection.py
@@ -4,7 +4,7 @@ import serial
 
 
 def send_arduino_message(serial_identifier: str, message: str):
-    serial_connection = serial.Serial(serial_identifier, baudrate=9600, timeout=0.5)
+    serial_connection = serial.Serial(serial_identifier, baudrate=9600, timeout=0.001)
     encoded_message = message.encode('ascii')
     serial_connection.write(encoded_message)
     response_part = serial_connection.readline().decode('ascii')
