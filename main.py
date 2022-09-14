49f6369043c118f5b73422977b2fa1e873622329
Fabio Greter <fabio.greter@bluewin.ch>
2022-09-14 20:41:29 +0200

    Fix debugging
    Allow no GPIO (=> no Lock device), devices except led controller are optional

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import subprocess
 import serial
 from typing import List
 
+from gpiozero import BadPinFactory
+
 from notecontrollers.history_controller import HistoryController
 from notecontrollers.single_note_controller import SingleNoteController
 from triggerables.devices.coffeemaker import CoffeeMaker
@@ -9,6 +11,9 @@ from ledcontroller.led_controller import LEDController
 from triggerables.devices.lock import Lock
 from triggerables.games.whackamole import WhackAMole
 from midimon import midimon
+
+# USB_DEVICE_PREFIX = 'ttyUSB'  # Linux
+USB_DEVICE_PREFIX = 'cu.usbserial'  # MacOS
 
 COFFEE_ARDUINO_FUNCTION = 'coffee-maker'
 LED_ARDUINO_FUNCTION = 'led-controller'
@@ -19,26 +24,33 @@ BAUD_RATES = [9600, 921600]
 def get_usb_devices() -> List[str]:
     ls_output = subprocess.check_output(['ls', '/dev']).decode('ascii')
     devices = ls_output.split('\n')
-    usb_device_list = ['/dev/' + device for device in devices if 'ttyUSB' in device]
+    usb_device_list = ['/dev/' + device for device in devices if USB_DEVICE_PREFIX in device]
     print('Detected USB devices:')
     print(usb_device_list)
     return usb_device_list
 
 
 def find_device_identifier_by_function(usb_device_list: List[str], function: str) -> str:
+    print('-' * 30)
+    print('Trying to find device identifier for', function)
     for device in usb_device_list:
+        print('Trying device', device)
         try:
             for baud_rate in BAUD_RATES:
                 try:
+                    print('Trying baud rate', baud_rate)
                     ser = serial.Serial(device, baudrate=baud_rate, timeout=0.5)
                     ser.write(b'Aget device funcZ')
                     response = ser.readline().decode('ascii')
                     if function in response:
                         ser.close()
+                        print('Found device', device)
                         return device
                 except UnicodeDecodeError:
+                    print('Wrong baud rate')
                     continue
         except serial.SerialException or serial.SerialTimeoutException:
+            print('Serial exception')
             continue
     raise ValueError('Device ' + function + ' not found')
 
@@ -46,24 +58,31 @@ def find_device_identifier_by_function(usb_device_list: List[str], function: str
 if __name__ == '__main__':
     # Find USB devices
     usb_devices = get_usb_devices()
-    led_controller_serial_identifier = find_device_identifier_by_function(usb_devices, LED_ARDUINO_FUNCTION)
-    coffee_maker_serial_identifier = find_device_identifier_by_function(usb_devices, COFFEE_ARDUINO_FUNCTION)
+    coffee_maker_serial_identifier = None
 
     # Init controllers
     controller = HistoryController()
+    led_controller_serial_identifier = find_device_identifier_by_function(usb_devices, LED_ARDUINO_FUNCTION)
     led_controller = LEDController(
         serial_identifier=led_controller_serial_identifier,
     )
     single_note_controller = SingleNoteController(led_controller)
 
     # Init triggerables
-    coffee_maker = CoffeeMaker(
-        serial_identifier=coffee_maker_serial_identifier,
-        led_controller=led_controller
-    )
-    controller.add_triggerable(coffee_maker)
-    lock = Lock()
-    controller.add_triggerable(lock)
+    try:
+        coffee_maker_serial_identifier = find_device_identifier_by_function(usb_devices, COFFEE_ARDUINO_FUNCTION)
+        coffee_maker = CoffeeMaker(
+            serial_identifier=coffee_maker_serial_identifier,
+            led_controller=led_controller
+        )
+        controller.add_triggerable(coffee_maker)
+    except ValueError:
+        print('WARNING: coffee maker arduino not found, proceeding without')
+    try:
+        lock = Lock()
+        controller.add_triggerable(lock)
+    except BadPinFactory:
+        print('WARNING: failed to initialize lock, something\'s wrong with GPIO')
     whack_a_mole = WhackAMole(led_controller)
     controller.add_triggerable(whack_a_mole)
     single_note_controller.add_game(whack_a_mole)
